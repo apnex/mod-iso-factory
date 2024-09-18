eda658a79af51ad3cebb780f0db2daa064efd192
root <aobersnel@apnex.com.au>
2024-09-18 19:12:36 +1000

    Updates..

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 locals {
 	project	= var.gcp_project_id
 	region	= var.gcp_region
-	zone	= var.gcp_zone
 }
 
 resource "google_pubsub_topic" "ipxe" {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,2 @@
 variable "gcp_project_id" {}
 variable "gcp_region" {}
-variable "gcp_zone" {}
